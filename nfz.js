8d30a07a1e835f7d17663824512408937b325c11
Neil <neilwchen@gmail.com>
2017-01-07 12:44:24 -0500

    Random nice/rude msgs pushed to popup

--- nfz.js
+++ nfz.js
@@ -12,26 +12,29 @@ const debugMessage = function debugMessage(msg) {
  *  Mouth off to the user.
  *  TODO: customize sass based on how many episodes are being incr/decremented
  */
-const sass = function sass(maxEps) {
+const sass = function sass(maxEps, lastMaxEps) {
 	rudeMsgs = [
 		"Wow, more episodes - real ambitious.",
-		"That's one way to change your future.",
-		"You're better than this."
-		"There must be something better than another episode."
-		"Don't do it. I believe in you."
-	]
+		"More episodes? That's one way to change your future.",
+		"More episodes? You're better than this.",
+		"There must be something better than another episode.",
+		"More episodes? Don't do it. I believe in you."
+	];
 	niceMsgs = [
 		"Fewer episodes? More love for you. <3",
-		"Smart choice. Smart you.",
-		"Welcome to the #noflixzone!",
+		"Fewer episodes? Smart choice. Smart you.",
+		"Fewer episodes! Welcome to the #noflixzone!",
 		"That's right - take those episodes down!",
-		"Good choice. Keep building that focus!"
-	]
-	
+		"Great, fewer episodes! Keep building that focus!"
+	];
+
+	let sassCaption = document.getElementById('sassCaption');	
 	if(maxEps > lastMaxEps) {
-		// TODO: push random from rudeMsgs to popup
+		debugMessage(['maxEps ', 'is being increased']);
+		sassCaption.innerHTML = rudeMsgs[Math.floor(Math.random() * rudeMsgs.length)];
 	} else if(maxEps < lastMaxEps) {
-		// TODO: push random from niceMsgs to popup
+		debugMessage(['maxEps ', 'is being decreased']);
+		sassCaption.innerHTML = niceMsgs[Math.floor(Math.random() * niceMsgs.length)];
 	}
 	
 }
@@ -39,6 +42,7 @@ const sass = function sass(maxEps) {
 /*
  *  Watch for changes in the maximum number of episodes dropdown.
  */
+var lastMaxEps = 1;
 document.addEventListener('DOMContentLoaded', function() {
 
 	var options = document.querySelector('#maxEps');
@@ -49,7 +53,12 @@ document.addEventListener('DOMContentLoaded', function() {
 	options.addEventListener('change', function(evt) {
 		
 		// Instruct the background script to modify its maxEps.
-		chrome.runtime.sendMessage({greeting: evt.srcElement.value}, function(response) {
+		let maxEps = evt.srcElement.value;
+		chrome.runtime.sendMessage({greeting: maxEps}, function(response) {
+			if(lastMaxEps != maxEps) {
+				sass(maxEps, lastMaxEps);
+				lastMaxEps = maxEps;
+			}
 			debugMessage(['evt sent']);
 		});
 
