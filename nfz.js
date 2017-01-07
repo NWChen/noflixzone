7990a48e32a9cd3fdb9ed74e7dd4c77ac3e5588c
Neil <neilwchen@gmail.com>
2017-01-07 03:49:04 -0500

    Add basic rude/nice messages to sass user

--- nfz.js
+++ nfz.js
@@ -9,6 +9,34 @@ const debugMessage = function debugMessage(msg) {
 }
 
 /*
+ *  Mouth off to the user.
+ *  TODO: customize sass based on how many episodes are being incr/decremented
+ */
+const sass = function sass(maxEps) {
+	rudeMsgs = [
+		"Wow, more episodes - real ambitious.",
+		"That's one way to change your future.",
+		"You're better than this."
+		"There must be something better than another episode."
+		"Don't do it. I believe in you."
+	]
+	niceMsgs = [
+		"Fewer episodes? More love for you. <3",
+		"Smart choice. Smart you.",
+		"Welcome to the #noflixzone!",
+		"That's right - take those episodes down!",
+		"Good choice. Keep building that focus!"
+	]
+	
+	if(maxEps > lastMaxEps) {
+		// TODO: push random from rudeMsgs to popup
+	} else if(maxEps < lastMaxEps) {
+		// TODO: push random from niceMsgs to popup
+	}
+	
+}
+
+/*
  *  Watch for changes in the maximum number of episodes dropdown.
  */
 document.addEventListener('DOMContentLoaded', function() {
