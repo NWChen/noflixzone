b9b8ad0f5fba5dd4f7fd72713d7295bf2a0d3d59
Neil <neilwchen@gmail.com>
2017-01-07 01:35:11 -0500

    Fixed asynchronous calls to chrome API

--- background.js
+++ background.js
@@ -1,98 +1,118 @@
+const DEBUG = 1;
+const MAX_EPS = 1;
+
+/*
+ *  Output a message to the console.
+ */
+const debugMessage = function debugMessage(caller, msg) {
+	if(DEBUG) {
+		var output = caller;
+		output += ": ";
+		msg.forEach(function(e) {
+			output += e;
+		});
+		console.log(output);
+	}
+};
+
 /*
  *  Asks content script to block the current page.
  */
 const blockPage = function blockPage(tabId) {
 	chrome.tabs.sendMessage(tabId, {action: 'block_page'}, function(response) {
-		console.log("Content script returned successfully with response: " + response);
+		debugMessage('blockPage()', ['content script returned successfully']);
 	});
-}
+};
 
 /*
- *  Gets the number of episodes watched so far.
+ *  Increments the number of episodes watched so far.
  */
-const getEpsWatched = function getEpsWatched() {
-	chrome.storage.sync.get('nfzEps', function(response) {
-		if(response['nfzEps']) {
-			console.log("nfzEps retrieved successfully with value: " + response['nfzEps']);
-			return response['nfzEps'];
-		}
-		return null;
+const incrementEps = function incrementEps(callback) {
+	chrome.storage.sync.get('nfzEps', function(nfzObj) {
+		let nfzEps = nfzObj['nfzEps']+1;
+		chrome.storage.sync.set({'nfzEps': nfzEps}, function(response) {
+			debugMessage('getEpsWatched()', ['successfully incremented nfzEps with value ', nfzEps]);
+			callback(nfzEps);
+		});
 	});
-}
-
-/*
- *  Gets the trackId of the last video watched.
- */
-const getLastEp = function getLastEp() {
-	chrome.storage.sync.get('nfzLastEp', function(response) {
-		if(response['nfzLastEp']) {
-			console.log("nfzLastEp retrieved successfully with value: " + response['nfzLastEp']);
-			return response['nfzLastEp'];
-		}	
-		return null;
-	});
-}
+};
 
 /*
  *  Grabs the trackId of the current URL.
- *  Credits to David Walsh (https://davidwalsh.name).
+ *  Credits to David Walsh (https://davidwalsh.name)
  */
 const getTrackId = function getTrackId(url) {
 	//name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
     var regex = new RegExp('[\\?&]' + 'trackId' + '=([^&#]*)');
     var results = regex.exec(url);
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-}
+};
 
 /*
  *  Watch all tabs for netflix. Should be called whenever tabs are updated.
  *  TODO: find a workaround for tabs[0], since currentWindow never seems to be true?
  *  TODO: make sure netflix is only part of the domain, not the path
- *  TODO: find a way around 'active: true', for background instances of Netflix
+ *  TODO: find a way around 'active: true', for background instances of Netflix - just if url matches?
  */
-const watchTabs = function watchTabs() {
+const pollTabs = function pollTabs(callback) {
 	chrome.tabs.query({active: true}, function(tabs) {
-		if(tabs[0].url.indexOf('netflix') > -1) {
-			return true;
-		}
-		return false;
+		 callback(tabs[0].url.indexOf('netflix') > -1);
 	});
-}
+};
 
 /*
  *  Classify a video as watched.
  *  TODO: add time elapsing condition
- * 	TODO: clear Storage on extension leaving
+ * 	TODO: clear Storage Area on extension leaving
  */
-const isWatched = function isWatched(trackId) {
-	var watched = false;
-	chrome.storage.sync.get('nfzTrackId', function(trackObj) {
-		if(trackObj['nfzTrackId'] != trackId) {
-			chrome.storage.sync.set({'nfzTrackId': trackId});	// Reset current trackId
-			console.log('isWatched() found a new episode being currently watched.');
-			return true;
+const isWatched = function isWatched(trackId, callback) {
+	var result = 0;	
+	chrome.storage.sync.get('nfzTrackId', function(response) {
+		if(response['nfzTrackId'] != trackId) {
+			chrome.storage.sync.set({'nfzTrackId': trackId}, function() {		// StorageArea functions are asynchronous, so everything should be done in the callback
+				debugMessage('isWatched()', ['found a new episode being currently watched.']);
+				callback(1);
+			});
+		} else {
+			callback(0);
 		}
-		console.log('isWatched() did not find a new episode being currently watched.');
-		return false;
 	});
-}
+};
 
-// Initialize values in Storage Area.
+/*
+ *  Initialize values in Storage Area.
+ */
 chrome.storage.sync.set({'nfzEps': 0}, function(response) {
-	console.log('nfzEps was successfully initialized.');
+	debugMessage('main', ['nfzEps was successfully initialized.']);
 });
 chrome.storage.sync.set({'nfzTrackId': 0}, function(response) {
-	console.log('nfzTrackId was successfully initialized.');
+	debugMessage('main', ['nfzTrackId was successfully initialized.']);
 });
 
 
-// Watch for changes in any tabs, so we can look out for Netflix instances in background tabs too.
+/*
+ *  Watch for changes in any tabs, so we can look out for Netflix instances in background tabs too.
+ *  TODO: stop asynchronous calls from repeating on tab updates
+ */
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	// JS lazy evaluation
-	//if(watchTabs && isWatched(getTrackId(tab.url))) {
-	if(watchTabs) {
-		if(isWatched(getTrackId(tab.url)))
-		// ...block if necessary...	
-			console.log("New episode!");	
-	}
+	debugMessage('main', ['listener has been invoked.']);
+
+	// Determine if queried tab has Netflix open
+	pollTabs(function(isNetflix) {
+		if(isNetflix) {
+
+			// Determine if an episode has just been watched.
+			isWatched(getTrackId(tab.url), function(response) {
+				if(response) {
+
+					// Check if the max number of episodes has been watched.
+					incrementEps(function(nfzEps) {
+						if(nfzEps > MAX_EPS) {
+							blockPage(tabId);
+						}
+					});		
+				}
+			});
+		}
+	});
 });
