13f5d3fad2746c5f942f72fab6fef373e3cd9c24
Neil <neilwchen@gmail.com>
2017-01-06 21:17:34 -0500

    Handle adding to DOM in one function

--- block.js
+++ block.js
@@ -1,19 +1,18 @@
-function blockScreen() {
-	document.body.innerHTML +=
-	// Full screen overlay
-	`
-	<div class="nfz-overlay">
-		<div class="nfz-overlay-content">
-			<h1>You know better.</h1>
-			<hr>
-			<h2>No more episodes until</h2>
-			<span id="nfz-deadline">tomorrow at 5:20pm.</span>
-		</div>
-		</div>
-	`	
-}
-
-chrome.runtime.onMessage.addListener(function(request, sender, response) {
-	blockScreen();
-	document.getElementByTagName('video').pause(); // pause video player
+//chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(
+	function(request, sender) {
+		console.log("Blocking page now.");
+		if(request.action=='block_page') {
+			document.body.innerHTML +=
+			`
+			<div class="nfz-overlay">
+				<div class="nfz-overlay-content">
+					<h1>You know better.</h1>
+					<hr>
+					<h2>No more episodes until</h2>
+					<span id="nfz-deadline">tomorrow at 5:20pm.</span>
+				</div>
+			</div>
+			`;
+		}
 });
